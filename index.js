9856a1ffabdf35b66cb46e5d70db3b810ea551ac
hadialhamza <hamzaglory@gmail.com>
2026-01-12 04:52:45 +0600

    solved all 10 problems

--- index.js
+++ index.js
@@ -16,7 +16,6 @@ function reverseString(input) {
 
   return finalString;
 }
-// console.log(reverseString("hello"));
 
 // Problem 2: Count Vowels in a String
 function vowelsCounter(input) {
@@ -38,7 +37,6 @@ function vowelsCounter(input) {
 
   return count;
 }
-// console.log(vowelsCounter("programming"));
 
 // Problem 3: Check for Palindrome
 function isPalindrome(input) {
@@ -60,7 +58,6 @@ function isPalindrome(input) {
   const checkIsPalindrome = input === revString;
   return checkIsPalindrome;
 }
-// console.log(isPalindrome("madam"));
 
 // Problem 4: Find the Maximum Number
 function maxNum(input) {
@@ -73,4 +70,96 @@ function maxNum(input) {
   const maxNumber = Math.max(...input);
   return maxNumber;
 }
-// console.log(maxNum([5, 1, 99, 101, 3]));
+
+// Problem 5: Remove Duplicates from an Array
+function removeDuplicates(input) {
+  // array validation
+  if (!Array.isArray(input)) {
+    return "Please enter a valid array.";
+  }
+
+  // remove duplicate items from array using Set constructor
+  const uniqueNumbs = new Set(input);
+
+  // make an array with unique numbers
+  const uniqueArray = [...uniqueNumbs];
+  return uniqueArray;
+}
+
+// Problem 6: Sum of All Numbers in an Array
+function sum(input) {
+  // array validation
+  if (!Array.isArray(input)) {
+    return "Please enter a valid array.";
+  }
+
+  // using reduce array method for total sum of the array
+  const totalSum = input.reduce((total, number) => total + number, 0);
+  return totalSum;
+}
+
+// Problem 7: Find Even Numbers in an Array
+function getEvenNumbers(input) {
+  // array validation
+  if (!Array.isArray(input)) {
+    return "Please enter a valid array.";
+  }
+
+  // using filter array method for finding the even numbers of the array
+  const evenNumber = input.filter((num) => num % 2 === 0);
+  return evenNumber;
+}
+
+// Problem 8: Capitalize First Letter of Each Word
+function capitalizeWord(input) {
+  // validation for input is a string
+  if (typeof input !== "string") {
+    return "Please enter a valid string";
+  }
+
+  // split the sentence into words
+  const words = input.split(" ");
+
+  // capitalize the first letter into uppercase
+  const capitalWords = words.map(
+    (word) => word.charAt(0).toUpperCase() + word.slice(1)
+  );
+
+  // join the words to make the sentence
+  const finalSentence = capitalWords.join(" ");
+  return finalSentence;
+}
+
+// Problem 9: Find the Factorial of a Number
+function factorial(input) {
+  // validation for number
+  if (typeof input !== "number") {
+    return "Please enter a valid number";
+  }
+
+  let result = 1;
+
+  // loop till input number and calculate factorial
+  for (let i = 1; i <= input; i++) {
+    result = result * i;
+  }
+  return result;
+}
+
+// Problem 10: PingPong Challenge
+function pingPong() {
+  // loop for 1 to 20
+  for (let i = 1; i <= 20; i++) {
+    // check the numbers for expected result
+    if (i % 3 === 0 && i % 5 === 0) {
+      console.log("PingPong");
+    } else if (i % 3 === 0) {
+      console.log("Ping");
+    } else if (i % 5 === 0) {
+      console.log("Pong");
+    } else {
+      console.log(i);
+    }
+  }
+}
+// pingPong();
