6e97e3b7153e0412996b2b52ac6660a13c228d36
hadialhamza <hamzaglory@gmail.com>
2026-01-12 02:15:00 +0600

    problem 3 and 4 solved.

--- index.js
+++ index.js
@@ -16,11 +16,9 @@ function reverseString(input) {
 
   return finalString;
 }
-
 // console.log(reverseString("hello"));
 
 // Problem 2: Count Vowels in a String
-
 function vowelsCounter(input) {
   // validation for input is a string and make the string to lowercase
   if (typeof input !== "string") {
@@ -42,5 +40,37 @@ function vowelsCounter(input) {
 }
 // console.log(vowelsCounter("programming"));
 
-
 // Problem 3: Check for Palindrome
+function isPalindrome(input) {
+  // validation for input is a string
+  if (typeof input !== "string") {
+    return "Please enter a valid string";
+  }
+
+  // 1. split the string into an array
+  const strArray = input.split("");
+
+  // 2. reverse the array
+  const revArray = strArray.reverse();
+
+  // 3. join the reversed array
+  const revString = revArray.join("");
+
+  // 4. match the input string with reverse string
+  const checkIsPalindrome = input === revString;
+  return checkIsPalindrome;
+}
+// console.log(isPalindrome("madam"));
+
+// Problem 4: Find the Maximum Number
+function maxNum(input) {
+  // array validation
+  if (!Array.isArray(input)) {
+    return "Please enter a valid array.";
+  }
+
+  // checking the maximum number in array
+  const maxNumber = Math.max(...input);
+  return maxNumber;
+}
+// console.log(maxNum([5, 1, 99, 101, 3]));
